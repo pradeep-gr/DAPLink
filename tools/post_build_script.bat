ff6cf4cab8647bd10311ed67a29fa6dcdc6b5d56
maclobdell <maclain.lobdell@arm.com>
2016-02-11 10:45:54 -0600

    updates for k20dx (on frdm-k64f) to interface to ncs36510

--- tools/post_build_script.bat
+++ tools/post_build_script.bat
@@ -6,5 +6,6 @@ set base_name= %1
 fromelf --bin %base_name%.axf -o %base_name%.bin
 fromelf --i32 %base_name%.axf -o %base_name%.hex
 python %TOOLS%\post_compute_crc.py %base_name%.hex %base_name%_crc.hex --bin %base_name%_crc.bin
+python %TOOLS%\offset_update.py --start 0x8000 --padded_start 0x5000 %base_name%_crc.bin --output %base_name%_crc_0x5000.bin
 REM Always return success
 exit 0
